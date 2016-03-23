1e0cbfe216868f9ac77386dd7d2f965937ef3cdf
Asanka Vithanage <gvasanka@gmail.com>
2016-03-23 12:27:49 +0530

    Adding new testcases

---
+++ src/test/java/org/asanka/tests/AsankaTest1.java
@@ -0,0 +1,19 @@
+package org.asanka.tests;
+
+import org.testng.annotations.*;
+
+/**
+ * Created by asankav on 3/23/16.
+ */
+public class AsankaTest1 {
+
+    @Test
+    public void autoTest1(){
+        assert (false);
+    }
+
+    @Test
+    public void autoTest2(){
+        assert (true);
+    }
+}
